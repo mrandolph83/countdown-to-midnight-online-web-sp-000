ab385742532252951031398b79c44bf8ba215341
Matt Smith <mrandolph83@gmail.com>
2020-09-11 15:58:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,17 @@
 #write your code here
-x = 10
-
-def countdown(x)
-while x > 0
-  returns "#{x} SECOND(S)!"
-  x -= 1
+def countdown(sec)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    sec -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
-  returns "HAPPY NEW YEAR!"
+
+def countdown_with_sleep(sec)
+  while seconds > 0
+    puts "#{sec} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
